ec4dfc289e57bd4ebafc542abc44207ddc0d5cab
Emmett Hoolahan <emmetthoolahan@gmail.com>
2024-12-21 18:12:48 +0800

    asd
    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import auth from 'auth-astro';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [auth()]
 });
