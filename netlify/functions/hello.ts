1f10cdf01c090300cf91b36dbf5a4a69fe24d3cd
Emmett Hoolahan <emmetthoolahan@gmail.com>
2024-12-21 19:57:47 +0800

    asd
    das

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,1 +1,8 @@
- 
+import type { Handler } from '@netlify/functions';
+
+export const handler: Handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello from Netlify Functions!' }),
+  };
+}; 
