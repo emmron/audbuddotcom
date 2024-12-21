e207411e8ed72f01a3a8d498f27fdfff30833c54
Emmett Hoolahan <emmetthoolahan@gmail.com>
2024-12-21 17:59:37 +0800

    qwdqwd
    wdqqwd

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly GITHUB_CLIENT_SECRET: string;
   readonly GOOGLE_CLIENT_ID: string;
   readonly GOOGLE_CLIENT_SECRET: string;
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
